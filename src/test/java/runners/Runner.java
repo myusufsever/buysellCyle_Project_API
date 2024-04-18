3248d19db5087e7432ca9ac97050729d41ae7e41
s-atalay <simgeeokurlar@gmail.com>
2024-04-19 00:32:00 +0300

    üçüncü

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/features/API", "src/test/resources/features/DB"},
         glue = {"stepdefinitions", "hooks"},
 
-        tags = "@[US_002-TC_002]",
+        tags = "@simge",
 
 
         dryRun = false
