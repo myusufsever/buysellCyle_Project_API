f1e1061a8adddc32b065924e9c169bc2b60ce52b
Zehra Demir <zdemir2702@gmail.com>
2024-04-25 10:24:49 +0300

    /25.04/zehra/5.v

--- src/test/java/manage/Manage.java
+++ src/test/java/manage/Manage.java
@@ -7,14 +7,17 @@ public class Manage {
     public String getBank_account_insert_data() { return bank_account_insert_data; }
     private String bank_account_insert_data="Insert Into bank_accounts (id,bank_name,branch_name,account_name,account_number,opening_balance,description,status) Values(?,?,?,?,?,?,?,?)";
 
-
-
     private String cities_veri_ekleme = "INSERT INTO cities (name, state_id, status, created_at) VALUES (?, ?, ?, ?);";
     public String getCities_veri_ekleme() {return cities_veri_ekleme;}
 
     private String  refund_reasons_null  ="SELECT * FROM refund_reasons where reason is null;";
     public String getRefund_reasons_null() {return refund_reasons_null;}
 
+    private String verify_seller_products = "SELECT * FROM u168183796_qabuysell.seller_products where discount = 0 limit 3;";
+    public String getVerify_seller_products() { return verify_seller_products; }
+
+    private String customerCouponStoresAndUsers  = "SELECT users.*, customer_coupon_stores.* FROM customer_coupon_stores LEFT JOIN users ON customer_coupon_stores.id = users.id LIMIT 3;";
+    public String getCustomerCouponStoresAndUsers() { return customerCouponStoresAndUsers; }
 
 
 
--- src/test/resources/features/DB/US_014.feature
+++ src/test/resources/features/DB/US_014.feature
@@ -1,11 +1,12 @@
-Feature: Verify whether there is data in the refund_reasons table with a 'reason' value of Null
+Feature: BACKEND JDBC TESTING
 
   Background:Database connection
     * Database connection is established.
 
     #refund_reasons tablosunda 'reason' degeri Null olan veri olup olmadigini dogrulayiniz.
   @DB_14
-  Scenario:
+  Scenario:Verify whether there is data in the refund_reasons table with a 'reason' value of Null
+
     * Verify whether there is data Query is prepared and executed.
     * Verify the "reason" information result are obtained.
     * Database connection is closed
